7ffdda326c145c662a7400b3cb7ed45e52e90c48
Aryan Devrani <aryand@xhdaryand40x.xlnx.xilinx.com>
2022-09-12 14:03:16 +0530

    Modified script to use Git path.

--- util/common/scripts/program_multicard.sh
+++ util/common/scripts/program_multicard.sh
@@ -8,6 +8,6 @@ num=0
 for i in "${TARGET_ID_LIST[@]}"
 do
 	#/proj/xbuilds/2019.1_daily_latest/installs/lin64/Vivado/2019.1/bin/vivado -nolog -nojournal -mode batch -source HEAD/proj/util/common/scripts/program_bitstream.tcl -tclargs ${BOARD_IDX_LIST[$num]} 0 ${BITFILE_LIST[$num]}
-	sh HEAD/proj/util/common/scripts/program_qdma_bitstream.sh ${TARGET_ID_LIST[$num]} ${BITFILE_LIST[$num]}
+	sh util/common/scripts/program_qdma_bitstream.sh ${TARGET_ID_LIST[$num]} ${BITFILE_LIST[$num]}
 	num=$((num+1))
 done
